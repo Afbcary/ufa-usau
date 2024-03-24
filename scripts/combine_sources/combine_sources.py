d172f043799f4ef8aabea7b327f2dd25b76af6a3
Austin Cary <afbcary@gmail.com>
2024-03-23 17:04:23 -0700

    add rankings data into combined dataset

--- scripts/combine_sources/combine_sources.py
+++ scripts/combine_sources/combine_sources.py
@@ -6,10 +6,11 @@ dirname = os.path.dirname(__file__).replace("/scripts/combine_sources","")
 
 club_df = pd.read_csv(f'{dirname}/data/club_data.csv')
 ufa_df = pd.read_csv(f'{dirname}/data/ufa_rosters.csv', header=None, names=['first_name','last_name','ufa_team'])
-# rankings_df = pd.read_csv(f'{dirname}/data/ufa_rosters.csv')
+men_rankings_df = pd.read_csv(f'{dirname}/data/frisbee_rankings_men.csv', header=None, names=['rank','club_team','rating', 'division'])
+mixed_rankings_df = pd.read_csv(f'{dirname}/data/frisbee_rankings_mixed.csv', header=None, names=['rank','club_team','rating', 'division'])
 
-combined_df = ufa_df.merge(club_df, on=['first_name','last_name'],how='left')
-# combined_df = combined_df.merge(rankings_df)
+combined_df = ufa_df.merge(club_df, on=['first_name','last_name'], how='left')
+combined_df = combined_df.merge(men_rankings_df, on=['club_team'], how='left')
+combined_df = combined_df.merge(mixed_rankings_df, on=['club_team'], how='left')
 
-print(combined_df.sort_values(by=['ufa_team','division','club_team','last_name']).head(50))
 combined_df.to_csv(f'{dirname}/data/combined_data.csv', index=False)
